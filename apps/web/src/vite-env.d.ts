0a670875fd8de7f0d61a651c8293ccbd0123ed91
Daniel Gavrilov <dgavrilov95@gmail.com>
2025-10-23 11:22:48 +0200

    Add tracking with posthog

--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string | undefined;
+  readonly VITE_SUPABASE_URL: string | undefined;
+  readonly VITE_SUPABASE_ANON_KEY: string | undefined;
+  readonly VITE_PUBLIC_POSTHOG_KEY: string;
+  readonly VITE_PUBLIC_POSTHOG_HOST: string;
+  // add more environment variables here as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
